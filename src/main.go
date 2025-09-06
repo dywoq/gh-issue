e8f8a57afa749b7256a231d54a81649aaae8a92f
dywoq <aleks.koyf@gmail.com>
2025-09-06 21:45:25 +0300

    [src] add pagesInitHome

--- src/main.go
+++ src/main.go
@@ -1,5 +1,21 @@
 package main
 
-func main() {
+import (
+	"log"
 
+	"github.com/rivo/tview"
+)
+
+func main() {
+	app := tview.NewApplication()
+	pages := tview.NewPages()
+
+	// initialization of pages
+	pagesInitHome(pages)
+
+	// starting
+	err := app.SetRoot(pages, true).EnableMouse(true).Run()
+	if err != nil {
+		log.Println(err)
+	}
 }
