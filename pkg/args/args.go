d7a06c9d1e94a5e6e87d025b8592b9952592b62c
dywoq <aleks.koyf@gmail.com>
2025-09-07 12:22:55 +0300

    [pkg/args] add command.go

--- pkg/args/args.go
+++ pkg/args/args.go
@@ -1,55 +1,23 @@
 package args
 
 import (
-	"errors"
-	"os"
-	"strconv"
-	"strings"
-
 	"github.com/dywoq/dywoqlib/lib/err"
+	"os"
 )
 
 // Args is a struct presenting arguments of gh-issue program.
 type Args struct {
-	Issues     []int64
-	Owner      string
-	Repository string
-	Token      string
+	Command Command
+	Args    []any
 }
 
 // New creates and returns a pointer to Args, along with the possible encountered error.
-// If len(os.Args) is not 5, it returns nil and a error.
 func New() (*Args, err.Context) {
-	if len(os.Args) != 5 {
-		return nil, err.NewContext(errors.New("github.com/dywoq/gh-issue/pkg/args: len(os.Args) is not 5"), "source is args.New() (*args.Args, err.Context)")
+	switch os.Args[1] {
+	case string(CommandGet):
+		return CommandProcessGet()
+	case string(CommandDelete):
+		return CommandProcessGet()
 	}
-	args := &Args{}
-	formattedIssues, err2 := issuesFormat(os.Args[1])
-	if err2 != nil {
-		copy := err.NoneContext()
-		copy.Copy(err2)
-		return nil, copy
-	}
-
-	args.Issues = formattedIssues
-	args.Owner = os.Args[2]
-	args.Repository = os.Args[3]
-	args.Token = os.Args[4]
-	return args, err.NoneContext()
-}
-
-func issuesFormat(arg string) ([]int64, err.Context) {
-	if arg == "" {
-		return []int64{}, err.NewContext(errors.New("github.com/gh-issue/pkg/args: the argument string is empty"), "source is args.issuesFormat(string) ([]int64, err.Context)")
-	}
-	split := strings.Split(arg, ",")
-	ids := []int64{}
-	for _, part := range split {
-		id, err2 := strconv.Atoi(part)
-		if err2 != nil {
-			return []int64{}, err.NewContext(err2, "source is args.issuesFormat(string) ([]int64, err.Context)")
-		}
-		ids = append(ids, int64(id))
-	}
-	return ids, err.NoneContext()
+	return nil, err.NoneContext()
 }
---
+++ pkg/args/command.go
@@ -0,0 +1,45 @@
+package args
+
+import (
+	"errors"
+	"os"
+
+	"github.com/dywoq/dywoqlib/lib/err"
+)
+
+type Command string
+
+const (
+	CommandGet    Command = "get"
+	CommandDelete Command = "delete"
+)
+
+func CommandProcessGet() (*Args, err.Context) {
+	if len(os.Args) != 5 {
+		err2 := err.NoneContext()
+		err2.SetError(errors.New("github.com/dywoq/gh-issue/pkg/args: len(os.Args) is not 5"))
+		err2.SetMore("source is args.CommandProcessGet() (*Args, err.Context)")
+		return nil, err2
+	}
+	args := &Args{}
+	args.Args[1] = os.Args[1] // issue ids
+	args.Args[2] = os.Args[2] // owner
+	args.Args[3] = os.Args[3] // repository
+	args.Args[4] = os.Args[4] // token
+	return args, err.NoneContext()
+}
+
+func CommandProcessDelete() (*Args, err.Context) {
+	if len(os.Args) != 5 {
+		err2 := err.NoneContext()
+		err2.SetError(errors.New("github.com/dywoq/gh-issue/pkg/args: len(os.Args) is not 5"))
+		err2.SetMore("source is args.CommandProcessDelete() (*Args, err.Context)")
+		return nil, err2
+	}
+	args := &Args{}
+	args.Args[1] = os.Args[1] // issue ids
+	args.Args[2] = os.Args[2] // owner
+	args.Args[3] = os.Args[3] // repository
+	args.Args[4] = os.Args[4] // token
+	return args, err.NoneContext()
+}
