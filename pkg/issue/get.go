7d36a8b046a3e541e95324c844c947fd7e999611
dywoq <aleks.koyf@gmail.com>
2025-09-07 13:10:46 +0300

    [pkg/issue] add function GetAllId

--- pkg/issue/get.go
+++ pkg/issue/get.go
@@ -21,3 +21,34 @@ func Get(owner, repo, token string, id int) (*github.Issue, err.Context) {
 	}
 	return issue, err.NoneContext()
 }
+
+// GetAllId gets all issue and returns their IDs.
+// its owner and id, and returns it.
+// If owner, repo or token is empty, it returns nil and error.
+func GetAllId(owner, repo, token string) ([]int, err.Context) {
+	if owner == "" || repo == "" || token == "" {
+		return []int{}, err.NewContext(errors.New("github.com/dywoq/gh-issue/pkg/issue: owner, repo or token is empty"), "source is issue.GetAllId(string, string, string, int) ([]int, err.Context)")
+	}
+	c, ctx := client(token)
+	result := []int{}
+	options := &github.IssueListByRepoOptions{
+		State: "all",
+		ListOptions: github.ListOptions{
+			PerPage: 100,
+		},
+	}
+	for {
+		issues, resp, err2 := c.Issues.ListByRepo(ctx, owner, repo, options)
+		if err2 != nil {
+			return []int{}, err.NewContext(err2, "source is issue.GetAllId(string, string, string) ([]int64, err.Context)")
+		}
+		for _, issue := range issues {
+			result = append(result, int(issue.GetID()))
+		}
+		if resp.NextPage == 0 {
+			break
+		}
+		options.ListOptions.Page = resp.NextPage
+	}
+	return result, err.NoneContext()
+}
